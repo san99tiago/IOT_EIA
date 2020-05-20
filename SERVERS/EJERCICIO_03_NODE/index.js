ccea8efd7052927c93690ea66247594f4da9d70f
san99tiago <san99tiago@gmail.com>
2020-05-20 01:43:13 -0500

    server with multiple colors on terminal

--- SERVERS/EJERCICIO_03_NODE/index.js
+++ SERVERS/EJERCICIO_03_NODE/index.js
@@ -1,5 +1,37 @@
 // ARCHIVO MAIN PARA CORRECTO FUNCIONAMIENTO DE NODEJS (Y SERVIDOR)
 //Santiago Garcia Arango
+//-----------------------------------------------------------------------------
+
+////TEST API PLUGIN GOOGLE MAPS
+// const {Client, Status} = require("@googlemaps/google-maps-services-js");
+// const client = new Client({});
+
+// client
+// .elevation({
+//     params: {
+//         locations: [{ lat: 45, lng: -110 }],
+//         keys: "asdf",
+//     },
+//     timeout: 1000, // milliseconds
+// })
+// .then((r) => {
+//     if (r.data.status === Status.OK) {
+//         console.log(r.data.results[0].elevation);
+//     } else {
+//         console.log(r.data.error_message);
+//     }
+// })
+// .catch((e) => {
+//     console.log(e);
+// });
+
+
+
+
+
+//------------------------------COLORS (terminal)---------------------------
+//Package that allows us to print with colors in terminals (more fancy)
+const colors = require('colors');
 
 //----------------------------------MYSQL---------------------------------------
 //Forma de conectarnos a Base de Datos de MySQL (ya creada en MySQL workbench)
@@ -256,6 +288,6 @@ app.post("/TEST_POST" ,function( request , response ) {
 // Esta parte permite indicar el procesamiento correcto, indicando puerto
 app.listen( 3000 , function() 
 {
-    console.log( "server ready... \n listening..." );
+    console.log( "server ready... \n listening...".bgGreen );
 } 
 );
