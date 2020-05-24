3e8cebce3b4555e478ca3f841a298f1c1828555c
san99tiago <san99tiago@gmail.com>
2020-05-24 04:59:23 -0500

    Extra gitignore para carpeta actual (incluir package-lock.json

--- SERVERS/EJERCICIO_04_TALLER_FINAL/my_routes/index.js
+++ SERVERS/EJERCICIO_04_TALLER_FINAL/my_routes/index.js
@@ -12,28 +12,52 @@ const express = require('express');
 const my_router = express.Router();
 
 
+//Nos permite obtener "parent directories" para re-dirigir mejor las rutas y archivos html
+var path = require("path");
+
+
+//----------------------------------MYSQL---------------------------------------
+//Forma de conectarnos a Base de Datos de MySQL (ya creada en MySQL workbench)
+//Creamos variable para menejar mysql a traves de dependencia ya instalada de mysql
+var mysql = require('mysql');
+
+//Creamos variable para "conectarnos" con mysql a la base de datos y tabla especifica
+var con = mysql.createConnection({
+    //Info necesaria para acceder a la base de datos respectiva
+    host: "localhost", 
+    user: "root",
+    password: "coco", 
+    database: "iot_taller_final" //Base de datos
+});
+
+
 //--------------------------------PROCESAMIENTO DE RUTAS----------------------------------
 //-----------------------------------------------------------------------------------------
 
 
 //----------------------------------LOGIN---------------------------------------------------
+//Variables importantes para saber usuario actual y verificar si ya ingreso correctamente
+var usuario_actual = "";
+var ingreso = false;
+
 //Principal path para redireccionar al "/login"
-app.get("/" ,function( request , response ) {
-    // De esta forma "renderiamos" archivo HTML
-    response.sendFile( __dirname + "/public/login.html");
+my_router.get("/" ,function( request , response ) {
+    ingreso = 0; //Reseteamos variable ingreso (forma de cerrar sesion desde "main.js")
+    usuario_actual = ""; //Reseteamos el nombre del usuario actual en la session
+
+    //Redirigimos archivo html hacia login.html
+    response.sendFile( path.join(__dirname,"..", "/public/login.html") );
     console.log("login_entry".green);
 });
 
-//Variable para procesar y validar el ingreso correcto
-var ingreso = false;
 
-app.post("/login",function(request,response) {
+my_router.post("/login",function(request,response) {
     //Almacenamos info del body del post (en este caso JSON es contenido de interes)
     let data = request.body;
     console.log(data);
 
     //Hacemos un query al MySQL pidiendo acceso a test_table_1 (usuario y password se remplazan por "user" y "pass" (sintaxis remplazo))
-    con.query('SELECT * FROM test_table_1 WHERE usuario = ? AND password = ?', [data.usuario, data.password], function(error, result, fields) { 
+    con.query('SELECT * FROM iot_taller_final.usuarios WHERE usuario = ? AND password = ?', [data.usuario, data.password], function(error, result, fields) { 
         
         //Creamos un objeto para recibir info de MySQL
         var info_obtenida = {};
@@ -45,6 +69,7 @@ app.post("/login",function(request,response) {
         //Validamos usuario
         if (info_obtenida.length > 0) {
             ingreso = true;
+            usuario_actual = data.usuario;
             response.send("server_ok");
         } else {   
             response.send('Usuario y/o contraseña incorrectos!');
@@ -61,19 +86,20 @@ app.post("/login",function(request,response) {
 
 //-----------------------------------------CHANGE PASSWORD-----------------------------------------------
 //Redireccion a url path para cambiar password
-app.get("/change_password" ,function( request , response ) {
+my_router.get("/change_password" ,function( request , response ) {
     // De esta forma "renderiamos" archivo HTML
     //Llevamos a HTML asociado    
-    response.sendFile( __dirname + "/public/change_password.html");
+    response.sendFile( path.join(__dirname ,"..", "/public/change_password.html") );
 
     //Mostramos en terimnal todo OK
     console.log("change_password_entry".red);
 });
 
-app.post("/change_password",function(request,response) {
+my_router.post("/change_password",function(request,response) {
 
     var data = request.body;
     console.log(data);
+
 
     if ( data.new_password_1 == data.new_password_2){
         console.log("CONTRASENNAS IGUALES");
@@ -117,7 +143,9 @@ app.post("/change_password",function(request,response) {
         console.log("CONTRASENNAS DIFERENTES");
     }
 
-    
+
+
+
 });
 
 
@@ -126,7 +154,54 @@ app.post("/change_password",function(request,response) {
 
 
 
+//-----------------------------------------ELIMINAR USUARIO-----------------------------------------------
+//Redireccion a url path para cambiar password
+my_router.get("/eliminar_usuario" ,function( request , response ) {
+    // De esta forma "renderiamos" archivo HTML
+    //Llevamos a HTML asociado    
+    response.sendFile( path.join(__dirname ,"..", "/public/eliminar_usuario.html") );
 
+    //Mostramos en terimnal todo OK
+    console.log("change_password_entry".red);
+});
+
+my_router.post("/eliminar_usuario",function(request,response) {
+
+    var data = request.body;
+    console.log(data);
+
+
+    if ( data.new_password_1 == data.new_password_2){
+        console.log("CONTRASENNAS IGUALES");
+        //Hacemos un query al MySQL pidiendo acceso a test_table_1 (usuario y password se remplazan por "user" y "pass" (sintaxis remplazo))
+        con.query('SELECT * FROM iot_taller_final.usuarios WHERE usuario = ? AND password = ?', [data.actual_usuario, data.actual_password], function(error, results, fields) { 
+            
+            //Creamos un objeto para recibir info de MySQL
+            var info_obtenida = {};
+            //almacenamos fila recibida  y la mostramos en terminal
+            var info_obtenida = results; 
+            console.log(info_obtenida);
+            //Validamos que Usuario y Contrasenna indicados sean correctos (para proceder a cambiarlos)
+            if (info_obtenida.length > 0) {
+                //Hacemos un query al MySQL pidiendo acceso a test_table_1 (usuario y password se remplazan por "user" y "pass" (sintaxis remplazo))
+                con.query('UPDATE iot_taller_final.usuarios SET `password` = ? WHERE (`usuario` = ? )', [data.new_password_1, data.actual_usuario], function(error, results, fields) { 
+                    
+                    //Creamos un objeto para recibir info de MySQL
+                    var info_obtenida = {};
+                    //almacenamos fila recibida  y la mostramos en terminal
+                    var info_obtenida = results; 
+                    console.log(info_obtenida);
+                    response.send("datos_ok");
+                });  
+            }else{
+                response.send("datos_error_auth");
+            }
+        });        
+    }else{
+        response.send("datos_error");
+        console.log("CONTRASENNAS DIFERENTES");
+    }
+});
 
 
 
@@ -137,19 +212,19 @@ app.post("/change_password",function(request,response) {
 
 
 //-----------------------------------------MAIN LOGIN TO INFO-----------------------------------------------
-app.get("/main",function(request,response) {
+my_router.get("/main",function(request,response) {
     //Solamente se ingresa al cuestionario si ya hubo login correcto con MysQL
     if (ingreso) {
         console.log("success_entry");
-        response.sendFile(__dirname + "/public/cuestionario.html");
+        response.sendFile( path.join(__dirname, "..", "/public/main.html") );
     } else {
-    response.send("No se ha autenticado");
+        response.send("Traidor, usted NO se ha autentificado, la próxima le cerramos la cuenta.");
     }
 });
 
 
 //------------------------------------ESP POST RECIBIR INFO TEMP HUM------------------------------------------
-app.post("/esp_post" ,function( request , response ) {
+my_router.post("/esp_post" ,function( request , response ) {
 
     let data = request.body;
 
