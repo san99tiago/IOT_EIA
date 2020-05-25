74b86d412dc1911c9bcaea16c7560a26fc916bb8
san99tiago <san99tiago@gmail.com>
2020-05-25 01:49:01 -0500

    Nuevo admin amestro para correcto cambio de otros usuarios

--- SERVERS/EJERCICIO_04_TALLER_FINAL/public/js/admin.js
+++ SERVERS/EJERCICIO_04_TALLER_FINAL/public/js/admin.js
@@ -1,6 +1,15 @@
 // ARCHIVO PARA PROCESAR LA PARTE PRINCIPAL DE LOS ADMIN DE LA APLICACION(UTILIZANDO JQUERY)
 //SANTIAGO GARCIA ARANGO
 //-------------------------------------------------------------------------------------------
+//Archivo JSON para procesar diferentes eventos segun los botones indicados (first_access nos ayuda a llenar tabla)
+var info_principal = {
+    first_access: "yes",
+    admin_que_hacer: "0",
+    admin_usuario_elegido: "0",
+    admin_dato_cambiar: "0",
+    admin_nuevo_dato: "0",
+};
+
 
 //-----------------------------BOTON DE SALIRSE-----------------------------------------------
 //Agregamos "EventListener" para procesar click en boton
@@ -9,21 +18,10 @@ document.getElementById('salir_admin').addEventListener('click', function() {
 });
 
 
-//------------------------------BOTON CAMBIAR PASSWORD-----------------------------------------
-//Agregamos "EventListener" para procesar click en boton
-document.getElementById('password_change_button_admin').addEventListener('click', function() {
-    window.location.replace('/change_password')
-    
-});
-
 
 
 //------------------------------MOSTRAR TABLA DINAMICA------------------------------------------
-var info_principal = {
-    first_access: "yes",
-};
-
-var vector_usuarios = {};
+var vector_usuarios = {}; //Nos permite recorrer vector de usuarios que nos llegue desde servidor principal
 
 function mostrar_tabla_segun_usuarios() {
     $.post({
@@ -61,6 +59,51 @@ function mostrar_tabla_segun_usuarios() {
 
 
 
+
+
+
+//-----------------------BOTON PARA PROCESAR NUEVO CAMBIO DE ADMIN------------------------
+
+//Agregamos "EventListener" para procesar click en boton
+document.getElementById('admin_actualizar_boton').addEventListener('click', function() {
+    info_principal.first_access = "no";
+    info_principal.admin_que_hacer = document.getElementById("opciones_1").value;
+    info_principal.admin_usuario_elegido = document.getElementById("usuario_elegido").value;
+    info_principal.admin_dato_cambiar = document.getElementById("opciones_2").value;
+    info_principal.admin_nuevo_dato = document.getElementById("nuevo_dato").value;
+
+
+    //De esta manera utilizamos JQUERY para realizar el POST
+    $.post({
+        //Redirigimos el path para acceder al login(esta vez con post)
+        url: "/admin",
+        
+        //Convertimos objeto de datos de usuario a formato JSON
+        data: JSON.stringify(info_principal),
+        //Indicamos Header para enviar info de tipo JSON
+        contentType: "application/json",
+        
+        //En caso de lograr correcto POST
+        //nota: (datos_Entrada es la respuesta enviada desde el index.js)
+        success: function(datosEntrada,status) {
+
+            //En caso de que se logre conexion y usuario sea valido, se redirecciona url
+            //nota: esta validacion se hace desde el "index.js"
+            if(datosEntrada === "datos_actualizados_ok") {
+                jConfirm("Datos actualizados correctamente!", "OPERACIÓN EXITOSA", function(r) {
+                    window.location.replace("/admin");
+                });            
+            }
+            else if (datosEntrada === "no_existe_usuario"){
+                jAlert("Usuario indicado NO existe","ATENCION");
+            }
+            else if (datosEntrada === "error_datos_usuario"){
+                jAlert("Error con datos ingresados","ERROR");
+            }
+
+        }
+    });
+});
 
 
 
